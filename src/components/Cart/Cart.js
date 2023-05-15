c6529191311a5633b04dbdd6e0aee8c322291c08
Arif Hossan <arif.hossan.eu@gmail.com>
2023-05-16 00:27:52 +0600

    conditonal css rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,43 +1,41 @@
-import React from 'react';
-
-const Cart = ({cart,handleRemoveCart}) => {
-    let message;
-    if(cart.length ===0){
-        message = <p>Please, Buy atleast one item</p>
-    }
-    else if(cart.length ===1){
-        message=<div><h3>Amar joono akta naw</h3>
-        <p>Tor jonno arekta nis</p></div>
-    }
-    else {
-        message = <p>Thanks for buying</p>
-    }
-    return (
-        <div>
-            <h3>Order Summary</h3>
-            <p>Quantity : {cart.length}</p>
-            {
-                cart.map(tshirt => <p key={tshirt._id}>
-                    {tshirt.name}
-                    <button onClick={()=>handleRemoveCart(tshirt)}>*</button></p>)
-            }
-            {
-                message
-            }
-            {/* turnery rendering */}
-            {
-                cart.length === 3 ? <p>Tinta kinteso ?</p> :<p>Kin, aro beshi kin</p>
-            }
-            {/* and (true) conditional rendering */}
-            {
-               cart.length ===2 && <h2>Double Products</h2>
-            }
-            {/* or (render as long as false) conditional rendering */}
-            {
-               cart.length ===4 || <h2>Onek hoise</h2>
-            }
-        </div>
+import React from "react";
+import "./Cart.css";
+const Cart = ({ cart, handleRemoveCart }) => {
+  let message;
+  if (cart.length === 0) {
+    message = <p>Please, Buy atleast one item</p>;
+  } else if (cart.length === 1) {
+    message = (
+      <div>
+        <h3>Amar joono akta naw</h3>
+        <p>Tor jonno arekta nis</p>
+      </div>
     );
+  } else {
+    message = <p>Thanks for buying</p>;
+  }
+  return (
+    <div>
+        {/* conditional css */}
+      <h3 className={cart.length === 2 ? "orange" : "purple"}>Order Summary</h3>
+      <p className={`bold ${cart.length === 2 ? 'blue':'yellow'}`}>
+        Quantity : {cart.length}
+      </p>
+      {cart.map((tshirt) => (
+        <p key={tshirt._id}>
+          {tshirt.name}
+          <button onClick={() => handleRemoveCart(tshirt)}>*</button>
+        </p>
+      ))}
+      {message}
+      {/* turnery rendering */}
+      {cart.length === 3 ? <p>Tinta kinteso ?</p> : <p>Kin, aro beshi kin</p>}
+      {/* and (true) conditional rendering */}
+      {cart.length === 2 && <h2>Double Products</h2>}
+      {/* or (render as long as false) conditional rendering */}
+      {cart.length === 4 || <h2>Onek hoise</h2>}
+    </div>
+  );
 };
 
 export default Cart;
